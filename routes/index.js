57ff9eda1c51e7d8848e0f19e806c58b2df42043
jumartim <juanmarmar@gmail.com>
2015-07-12 15:52:49 +0200

    Añadidas preguntas y páginas
    Se añaden preguntas y páginas nuevasa quiz

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Bien venido a Quiz' });
 });
+
+router.get('/quizes/question',quizController.quetion);
+
+route.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
