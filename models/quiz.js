20ae667e34b4bc3d9dde57950327eb11acff4078
jumartim <juanmarmar@gmail.com>
2015-08-01 16:40:46 +0200

    Update quiz.js

--- models/quiz.js
+++ models/quiz.js
@@ -1,11 +1,11 @@
-//Definición del MDD
+//DefiniciÃ³n del MDD
 
 module.exports=function(sequelize, DataTypes){
 	return sequelize.define('Quiz',{
-		pregunta:{type:DataTypes.STRING, 
+		pregunta:{type: DataTypes.STRING, 
 				  validate:{notEmpty:{msg: "-->Falta pregunta"}}
 				  },
-		respuesta:{type:DataTypes.STRING,
+		respuesta:{type: DataTypes.STRING,
 					validate: {notEmpty:{msg:"-->Falta Respuesta"}}
 				  }
 	});
